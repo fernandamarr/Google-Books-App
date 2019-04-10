dd59cfb22aa27d030a58da7dde34fa8cf6657218
Fernanda Marroquin <fernanda.marroquin07@gmail.com>
2019-04-10 16:08:08 -0400

    delete btn

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
+import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Search} />
           <Route exact path="/search" component={Search} />
+          <Route exact path="/books/:id" component={Detail} />
           <Route exact path="/saved" component={Saved} />
           <Route component={NoMatch} />
         </Switch>
--- client/src/components/DeleteBtn/index.js
+++ client/src/components/DeleteBtn/index.js
@@ -3,9 +3,17 @@ import "./style.css";
 
 // The ...props means, spread all of the passed props onto this element
 // That way we don't have to define them all individually
+// function DeleteBtn(props) {
+//   return (
+//     <button className="btn" onClick={() => props.deleteBook(props)}>Delete Book</button>
+//   );
+// }
+
 function DeleteBtn(props) {
   return (
-    <button className="btn" onClick={() => props.deleteBook(props)}>Delete Book</button>
+    <span className="delete-btn" {...props} role="button" tabIndex="0">
+      ✗
+    </span>
   );
 }
 
